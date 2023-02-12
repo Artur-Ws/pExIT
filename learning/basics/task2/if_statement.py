58073650c0b5ac86d592ddf32d9a3ced4bc500cb
Artur-Ws <wysywysy97@gmail.com>
2023-02-12 19:51:06 +0100

    add first two tasks

---
+++ learning/basics/task2/if_statement.py
@@ -0,0 +1,14 @@
+'''
+Create new file "<initials>-<task_name>" in this directory, there you should solve this task.
+This file should stay unchanged! Example: "AW-input_print.py"
+
+You should know if-statement before this task. Example:
+if variable == 5:
+    print("It is 5!")
+else:
+    print("It is not 5!")
+
+Use code your from task1, extend it to check if name provided by user is valid (not empty, string-type),
+if not, print message that name is not valid. then ask if user wants to multiply, or add numbers,
+use if statement to perform correct operation.
+'''
