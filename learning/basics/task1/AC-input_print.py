3e333569ca7d34e93e0b947c09b611575572d70b
Artur C <arszil12345@gmail.com>
2023-02-13 20:39:43 +0100

    AC/Creating task_1 code

---
+++ learning/basics/task1/AC-input_print.py
@@ -0,0 +1,18 @@
+x = input("Hello! What is your name? ")
+print("Hello " + x + "!, give me two numbers and i will multiply them.")
+y = 0
+z = 0
+while y == 0:
+    y_str = input("First -> ")
+    try:
+        y = int(y_str)
+    except:print("Sorry, numbers only :)")
+
+while z == 0:
+    z_str = input("Second -> ")
+    try:
+        z = int(z_str)
+    except:print("Sorry, numbers only :)")
+
+
+print("Multiplication of your numbers is " + str(y * z))
