ebfdbe7a54459346dac31c96ed5120ea74296997
Pawel Kurowski <mietuski@gmail.com>
2023-02-13 22:38:15 +0100

    Solution of the first task

---
+++ learning/basics/task1/input_print_solved.py
@@ -0,0 +1,7 @@
+name = input("Hello! What is your name? ")
+
+number_1 = int(input("Type first number to multiplication "))
+
+number_2 = int(input("Type second number to multiplication "))
+
+print(f"{name}, your result of multiplication for {number_1}, and {number_2} is  {number_1 * number_2}")
