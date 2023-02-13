6df89c9b83715e8cf764816152d4c0b7d76ee3e6
Artur C <arszil12345@gmail.com>
2023-02-13 22:04:39 +0100

    Resolving infinite loop problem

--- learning/basics/task1/AC-input_print.py
+++ learning/basics/task1/AC-input_print.py
@@ -1,14 +1,14 @@
 x = input("Hello! What is your name? ")
 print(f"Hello {x}!, give me two numbers and i will multiply them.")
-y = 0
-z = 0
-while y == 0:
+y = None
+z = None
+while y == None:
     y_str = input("First -> ")
     try:
         y = int(y_str)
     except:print("Sorry, numbers only :)")
 
-while z == 0:
+while z == None:
     z_str = input("Second -> ")
     try:
         z = int(z_str)
