6ddfd75eef62194f16551e47bd3c76443824e036
Mateusz laptop <malicki9mateusz4@gmail.com>
2023-02-14 14:05:30 +0100

    Code refactored into f-strings

--- learning/basics/task1/MM-input_print.py
+++ learning/basics/task1/MM-input_print.py
@@ -19,9 +19,9 @@ class InputPrinter():
 
     def multiplier(self):
 
-        first_number = input("Hello " + self.name + ", give me first number to multiply! ")
-        second_number = input("now give me second number. ")
-        print(self.name + ", your result of multiplication for " + first_number + " and " + second_number + " is " + str(int(first_number)*int(second_number)) + "!")
+        first_number = int(input(f"Hello {self.name}, give me first number to multiply! "))
+        second_number = int(input("now give me second number. "))
+        print(f"{self.name}, your result of multiplication for {first_number}  and {second_number} is {first_number}*{second_number}.")
 
 
 if __name__ == "__main__":
