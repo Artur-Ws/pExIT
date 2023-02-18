1c595ffe27d8e9fb3b709705739c206c48f8ea69
Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>
2023-02-18 23:13:53 +0100

    Add Task1

---
+++ learning/basics/task1/PW-input_print.py
@@ -0,0 +1,9 @@
+name = str(input("Hi there. What's your name? "))
+
+print(f"Nice to meet you {name}. I will multiply two numbers for you.")
+num_1 = float(input("The first number is: "))
+num_2 = float(input("The second number is: "))
+
+result = num_1 * num_2
+
+print (f"The result of multiplying {num_1} and {num_2} is {result}")
