783896bb7804a83399dbcc0d17763652d131d078
Artur-Ws <wysywysy97@gmail.com>
2023-02-21 20:40:11 +0100

    new feature

--- playground/AW/conflict_presentation.py
+++ playground/AW/conflict_presentation.py
@@ -1,6 +1,8 @@
-def calculate(choice, number1, number2):
-  if choice.lower() == "a":
-      result = number1 + number2
-  if choice.lower() == "d":
+def calculate(operation, number1, number2):
+  if operation.lower() == 'a':
+    result = number1 + number2
+  if operation.lower() == 'm':
+    result = number1 * number2
+  if operation.lower() == "d":
       result = number1 / number2
   return result
