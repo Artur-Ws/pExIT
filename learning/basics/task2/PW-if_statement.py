01723a2463b77d2fd4ed1b13d10f9364abbf1d62
Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>
2023-02-25 22:31:57 +0100

    While loop for name and choice of A or M

--- learning/basics/task2/PW-if_statement.py
+++ learning/basics/task2/PW-if_statement.py
@@ -1,37 +1,35 @@
 name = input("Hi there. What's your name? ")
 
-if name == "":
+while name == "" or not name.isalpha():
+    if name == "":
 
-    print("Give me at least one letter")
+        name = input("Give me at least one letter ")
 
-elif not name.isalpha():
-    print("You've added at least one digit to your name (by accident I hope)")
+    elif not name.isalpha():
+        name = input("You've added at least one digit to your name (by accident I hope). Try again ")
 
-else:
-    print(f"Nice to meet you {name}")
+print(f"Nice to meet you {name}")
 
-    maths_op = str(input("Wanna multiply two numbers or add them? If multiply type M, If addition type A: "))
+maths_op = input("Wanna multiply two numbers or add them? For multiplication type M, for addition type A: ")
 
-    if maths_op == str("M"):
+while not (maths_op == str("A") or maths_op == str("M")):
+    maths_op = input("For multiplication type M, for addition type A ")
 
-        num_1 = float(input("Ok then. Give me first number: "))
+if maths_op == str("M"):
+    num_1 = float(input("Ok then. Give me the first number: "))
 
-        num_2 = float(input("The second number is: "))
+    num_2 = float(input("The second number is: "))
 
-        result = num_1 * num_2
+    result = num_1 * num_2
 
-        print(f"The result of multiplying {num_1} and {num_2} is {result}")
+    print(f"The result of multiplying {num_1} and {num_2} is {result}")
 
-    elif maths_op == str("A"):
+elif maths_op == str("A"):
 
-        num_1 = float(input("Ok then. Give me first number: "))
+    num_1 = float(input("Ok then. Give me the first number: "))
 
-        num_2 = float(input("Ok then. Give me first number: "))
+    num_2 = float(input("Ok then. Give me the second number: "))
 
-        result = num_1 + num_2
+    result = num_1 + num_2
 
-        print(f"The result of addition {num_1} and {num_2} is {result}")
-
-    else:
-
-        print("It in not an option. Good night.")
+    print(f"The result of addition {num_1} and {num_2} is {result}")
