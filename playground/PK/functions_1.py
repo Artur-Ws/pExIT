57b94fc66350b5867f2c94fecb0fdee4643f6f94
Pawel Kurowski <mietuski@gmail.com>
2023-02-25 22:00:19 +0100

    First task with functions

---
+++ playground/PK/functions_1.py
@@ -0,0 +1,20 @@
+def greeting(name):
+    print(f"Hello {name}, nice to meet you")
+
+names = ["Patrycja", "Mieszko", "Paweł"]
+
+#two ways to print greeting, using names from tupple
+
+print(names)
+
+for name in names:
+    
+    greeting(name)
+
+i = 0
+
+while i < 3:
+
+    greeting(names[i])
+
+    i += 1
