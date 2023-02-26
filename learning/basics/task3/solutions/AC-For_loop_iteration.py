55769d7bb24409f58a554b49c6870b6c1d676f4d
Artur C <arszil12345@gmail.com>
2023-02-26 20:33:53 +0100

    Change for review

--- learning/basics/task3/solutions/AC-For_loop_iteration.py
+++ learning/basics/task3/solutions/AC-For_loop_iteration.py
@@ -12,10 +12,7 @@ while True:
         else:
             print("This number is not within range, please enter a valid number.!")
 
-for i in prime_numbers:
-    if user_type in prime_numbers:
-        print(f"{user_type} is prime!")
-        break
-    else:
-        print(f"{user_type} is complex!")
-        break
+if user_type in prime_numbers:
+    print(f"{user_type} is prime!")
+else:
+    print(f"{user_type} is complex!")
