b72d7ed3812f047247d8b946ac79c05147a8c859
Ewelina Misiewicz <125140647+EweMis@users.noreply.github.com>
2023-02-26 20:11:47 +0100

    Add task1 (#27)

---
+++ learning/basics/task1/solutions/EM-input_print.py
@@ -0,0 +1,17 @@
+name = input("Hello! What is your name? ")
+
+print(f"Nice to meet you {name}! I will multiply two numbers for you. ")
+
+number_1 = int(input("Your first number "))
+
+number_2 = int(input("Your second number "))
+
+print(f"{name} The result of the multiplication is {number_1*number_2}")
+
+
+
+
+
+
+
+
