7de52daefb039ae1f30ed867e52d3d7cfc930324
Roskosz <jjroskosz@wp.pl>
2023-02-26 14:57:46 +0100

    part of second task

---
+++ learning/basics/task2/JR-input_print.py
@@ -0,0 +1,25 @@
+'''
+Create new file "<initials>-<task_name>" in this directory, there you should solve this task.
+This file should stay unchanged! Example: "AW-input_print.py"
+
+You should know if-statement before this task. Example:
+if variable == 5:
+    print("It is 5!")
+else:
+    print("It is not 5!")
+
+Use code your from task1, extend it to check if name provided by user is valid (not empty, string-type),
+if not, print message that name is not valid. then ask if user wants to multiply, or add numbers,
+use if statement to perform correct operation.
+
+'''
+
+name = input("Hello! What is your name? ")
+
+if
+
+number_1 = int(input("Type first number to multiplication "))
+
+number_2 = int(input("Type second number to multiplication "))
+
+print(f"{name}, your result of multiplication for {number_1}, and {number_2} is {number_1 * number_2}")
