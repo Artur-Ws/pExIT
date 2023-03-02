63932ae49244e6612ec17dc7e52919fd48b984ac
cichyc123 <124814739+cichyc123@users.noreply.github.com>
2023-03-02 19:46:13 +0100

    Added new file and solution to task3 (#31)
    * Added new file and solution to task3
    
    * Change for review

---
+++ learning/basics/task3/solutions/AC-For_loop_iteration.py
@@ -0,0 +1,18 @@
+
+prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+while True:
+    try:
+        user_type = int(input("Type a number in range 1-100 -> "))
+    except ValueError:
+        print("NUMBER please!")
+    else:
+        if user_type in range(1, 100):
+            break
+        else:
+            print("This number is not within range, please enter a valid number.!")
+
+if user_type in prime_numbers:
+    print(f"{user_type} is prime!")
+else:
+    print(f"{user_type} is complex!")
