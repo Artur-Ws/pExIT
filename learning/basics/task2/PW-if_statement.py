dac86b40631ee8628043cde279af0304e7ee7588
Patrycja Wysocka <wys.patrycja@gmail.com>
2023-03-07 21:38:58 +0100

    PW/Task2_from Master branch (#22)
    * Added Task2_from Master branch
    
    * While loop for name and choice of A or M
    
    * Remove converting string to string
    
    * Remove converting string to string
    
    ---------
    
    Co-authored-by: Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>

---
+++ learning/basics/task2/PW-if_statement.py
@@ -0,0 +1,35 @@
+name = input("Hi there. What's your name? ")
+
+while name == "" or not name.isalpha():
+    if name == "":
+
+        name = input("Give me at least one letter ")
+
+    elif not name.isalpha():
+        name = input("You've added at least one digit to your name (by accident I hope). Try again ")
+
+print(f"Nice to meet you {name}")
+
+maths_op = input("Wanna multiply two numbers or add them? For multiplication type M, for addition type A: ")
+
+while not (maths_op == "A" or maths_op == "M"):
+    maths_op = input("For multiplication type M, for addition type A ")
+
+if maths_op == "M":
+    num_1 = float(input("Ok then. Give me the first number: "))
+
+    num_2 = float(input("The second number is: "))
+
+    result = num_1 * num_2
+
+    print(f"The result of multiplying {num_1} and {num_2} is {result}")
+
+elif maths_op == "A":
+
+    num_1 = float(input("Ok then. Give me the first number: "))
+
+    num_2 = float(input("Ok then. Give me the second number: "))
+
+    result = num_1 + num_2
+
+    print(f"The result of addition {num_1} and {num_2} is {result}")
