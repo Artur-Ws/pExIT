dd5f1443160c94abfdb1cc03bad750e6cd41c590
Pawel Kurowski <mietuski@gmail.com>
2023-03-12 19:12:03 +0100

    Add init method

--- playground/PK/oop_first_lesson.py
+++ playground/PK/oop_first_lesson.py
@@ -3,20 +3,36 @@ age = 450
 class User:
     age = 0
     name = ""
+    def __init__(self, x, y):
+        print("initialization")
+
+        self.age = x
+        self.name = y
 
     def print_age(self, message):
         print(f"name: {self.name}, age: {self.age}, message: {message}")
     
-    def another_method(self):
-        pass
+user_1 = User(21, 'Areczek')
+user_2 = User(37, "Mireczek")
 
-user_1 = User()
-user_2 = User()
+# user_1.name = "Areczek"
+# user_1.age = 21
+# user_1.print_age("sample text")
 
-user_1.name = "Areczek"
-user_1.age = 21
+# user_2.name = "Mireczek"
+# user_2.age = 37
+# user_2.print_age("sample text")
+
+# userList = [User(), User()]
+
+# userList[0].age = 21
+# userList[1].age = 37
+# user_1.print_age("sample text")
+
+# userList[0].name = 'Areczek'
+# userList[1].name = "Mireczek"
+# user_2.print_age("sample text")
+
 user_1.print_age("sample text")
 
-user_2.name = "Mireczek"
-user_2.age = 37
 user_2.print_age("sample text")
