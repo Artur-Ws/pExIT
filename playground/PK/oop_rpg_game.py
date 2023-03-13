5ca3f04c45fed55b9be330deeb2009d2640c9616
Pawel Kurowski <mietuski@gmail.com>
2023-03-13 22:36:03 +0100

    Updating code just before second class

--- playground/PK/oop_rpg_game.py
+++ playground/PK/oop_rpg_game.py
@@ -3,15 +3,45 @@ class Character:
         self.name = name
         self.health_points = healt_points
         self.strenght = strenght
+        self.is_alive = True
+        self.backpack = []
     
     def get_damage(self, damage):
         self.health_points = self.health_points - damage
+        print(f"{self.name} taken {damage} damage. {self.health_points} health left.")
+        if self.health_points <= 0:
+            print(f"{self.name} has died.")
+            self.is_alive = False
 
-#inicialisation
+class Iteam:
+    def __init__(self, name, value, drop_chance):
+        self.name = name
+        self.value = value
+        self.drop_chance = drop_chance
+
+    def add_iteam(self, target):
+        target.backpack.append(self)
+
 player = Character("Player", 100, 20)
 enemy1 = Character("Orc", 80, 25)
 enemy2 = Character("Goblin", 20, 8)
-print(player.name, player.health_points, player.strenght)
-print(enemy2.health_points)
-player.get_damage(30)
-print(player.health_points)
+
+enemies = [enemy1, enemy2]
+
+while player.health_points > 0:
+    action = input("What you want to do? (A - attack)")
+
+    if action.lower() == "a":
+        enemies_left = [f"{i+1}: {enemy.name} - HP:{enemy.health_points}, DMG:{enemy.strenght}" for i, enemy in enumerate(enemies)]
+        print(enemies_left)
+        choice = input(f"Who you want to attack?\n{enemies_left}\n")
+        to_attack = enemies[int(choice) - 1]
+        to_attack.get_damage(player.strenght)
+        if to_attack.is_alive:
+            player.get_damage[to_attack.strenght]
+
+    for i, enemy in enumerate(enemies):
+        if not enemy.is_alive:
+            enemies.pop(i)
+
+print("GAME OVER")
