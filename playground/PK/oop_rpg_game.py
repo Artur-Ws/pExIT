021088e8fd6a59d00f4369ab76a906e75d64257d
Pawel Kurowski <mietuski@gmail.com>
2023-03-19 17:20:48 +0100

    Add critical strike

--- playground/PK/oop_rpg_game.py
+++ playground/PK/oop_rpg_game.py
@@ -1,17 +1,33 @@
+from random import randint
 class Character:
-    def __init__(self, name, healt_points, strenght):
+    def __init__(self, name, healt_points, strenght, crit_chacne):
         self.name = name
         self.health_points = healt_points
         self.strenght = strenght
+        self.crit_chance = crit_chacne
         self.is_alive = True
         self.backpack = []
+        
     
-    def get_damage(self, damage):
-        self.health_points = self.health_points - damage
-        print(f"{self.name} taken {damage} damage. {self.health_points} health left.")
-        if self.health_points <= 0:
-            print(f"{self.name} has died.")
-            self.is_alive = False
+    #is it ok to add crit strike like that?
+    def get_damage(self, damage, crit_luck):
+        crit_randomise = randint(1,100)
+        print(f"Attacker crit chance is: {crit_randomise}")
+        if crit_luck >= crit_randomise:
+            print(f"CRITICAL STRIKE LOADED")
+            damage *= 2
+            self.health_points = self.health_points - damage
+            print(f"{self.name} taken {damage} damage. {self.health_points} health left.")
+            if self.health_points <= 0:
+                print(f"{self.name} has died.")
+                self.is_alive = False
+        else:
+            print(f"CRITICAL STRIKE NOT LOADED")
+            self.health_points = self.health_points - damage
+            print(f"{self.name} taken {damage} damage. {self.health_points} health left.")
+            if self.health_points <= 0:
+                print(f"{self.name} has died.")
+                self.is_alive = False
 
 class Iteam:
     def __init__(self, name, value, drop_chance):
@@ -22,13 +38,20 @@ class Iteam:
     def add_iteam(self, target):
         target.backpack.append(self)
 
-player = Character("Player", 100, 20)
-enemy1 = Character("Orc", 80, 25)
-enemy2 = Character("Goblin", 20, 8)
+golden_ring = Iteam("Golden ring", 60, 25)
+silver_ring = Iteam("Silver ring", 60, 45)
+golden_necklace = Iteam("Golden necklace", 120, 10)
+gold_pouch = Iteam("Gold pouch", 10, 60)
+
+player = Character("Player", 100, 25, 50)
+enemy1 = Character("Orc", 80, 20, 25)
+enemy2 = Character("Goblin", 20, 8, 12)
 
 enemies = [enemy1, enemy2]
+items = [golden_ring, silver_ring, golden_necklace, gold_pouch]
 
 while player.health_points > 0:
+    print(f"Your backpack contains{[item for item in player.backpack]}")
     action = input("What you want to do? (A - attack)")
 
     if action.lower() == "a":
@@ -36,12 +59,19 @@ while player.health_points > 0:
         print(enemies_left)
         choice = input(f"Who you want to attack?\n{enemies_left}\n")
         to_attack = enemies[int(choice) - 1]
-        to_attack.get_damage(player.strenght)
+        to_attack.get_damage(player.strenght, player.crit_chance)
         if to_attack.is_alive:
-            player.get_damage[to_attack.strenght]
+            player.get_damage(to_attack.strenght, to_attack.crit_chance)
 
     for i, enemy in enumerate(enemies):
         if not enemy.is_alive:
             enemies.pop(i)
+            loot = []
+            for item in items:
+                luck = randint(1,100)
+                print(luck)
+                if luck <= item.drop_chance:
+                    item.add_iteam(player)
+
 
 print("GAME OVER")
