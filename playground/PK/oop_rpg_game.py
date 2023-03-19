565dbd02446c97ff882c0909e717ca31400c4c3f
Pawel Kurowski <mietuski@gmail.com>
2023-03-19 19:50:16 +0100

    Add dodge ability

--- playground/PK/oop_rpg_game.py
+++ playground/PK/oop_rpg_game.py
@@ -1,14 +1,15 @@
 from random import randint
 class Character:
-    def __init__(self, name, healt_points, strenght, crit_chacne):
+    def __init__(self, name, healt_points, strenght, crit_chacne, agility, defend):
         self.name = name
         self.health_points = healt_points
         self.strenght = strenght
         self.crit_chance = crit_chacne
+        self.agility = agility
+        self.defend = defend
         self.is_alive = True
         self.backpack = []
         
-    
     #is it ok to add crit strike like that?
     def get_damage(self, damage, crit_luck):
         crit_randomise = randint(1,100)
@@ -16,7 +17,7 @@ class Character:
         if crit_luck >= crit_randomise:
             print(f"CRITICAL STRIKE LOADED")
             damage *= 2
-            self.health_points = self.health_points - damage
+            self.health_points = self.health_points - damage #self.health_points = self.health_points - (damage * 2) #
             print(f"{self.name} taken {damage} damage. {self.health_points} health left.")
             if self.health_points <= 0:
                 print(f"{self.name} has died.")
@@ -28,7 +29,19 @@ class Character:
             if self.health_points <= 0:
                 print(f"{self.name} has died.")
                 self.is_alive = False
-
+        
+    def dodge(self):
+        dodge_luck = randint(1,100)
+        print(f"Dodge luck: {dodge_luck}")
+        
+        if self.agility >= dodge_luck:
+            print(f"{self.name} DODGE!")
+            return True
+        
+        else:
+            print(f"{self.name} Not dodged")
+            return False
+        
 class Iteam:
     def __init__(self, name, value, drop_chance):
         self.name = name
@@ -43,11 +56,12 @@ silver_ring = Iteam("Silver ring", 60, 45)
 golden_necklace = Iteam("Golden necklace", 120, 10)
 gold_pouch = Iteam("Gold pouch", 10, 60)
 
-player = Character("Player", 100, 25, 50)
-enemy1 = Character("Orc", 80, 20, 25)
-enemy2 = Character("Goblin", 20, 8, 12)
+player = Character("Player", 100, 25, 50, 50, 50)
+enemy1 = Character("Orc", 80, 20, 25, 25, 25)
+enemy2 = Character("Goblin", 20, 8, 12, 12, 12)
+enemy3 = Character("Sponge Bob", 1000, 1, 75, 75, 75)
 
-enemies = [enemy1, enemy2]
+enemies = [enemy1, enemy2, enemy3]
 items = [golden_ring, silver_ring, golden_necklace, gold_pouch]
 
 while player.health_points > 0:
@@ -56,12 +70,22 @@ while player.health_points > 0:
 
     if action.lower() == "a":
         enemies_left = [f"{i+1}: {enemy.name} - HP:{enemy.health_points}, DMG:{enemy.strenght}" for i, enemy in enumerate(enemies)]
-        print(enemies_left)
         choice = input(f"Who you want to attack?\n{enemies_left}\n")
         to_attack = enemies[int(choice) - 1]
-        to_attack.get_damage(player.strenght, player.crit_chance)
+        
+        if to_attack.dodge():
+            pass
+
+        else:
+            to_attack.get_damage(player.strenght, player.crit_chance)
+
         if to_attack.is_alive:
-            player.get_damage(to_attack.strenght, to_attack.crit_chance)
+
+            if player.dodge():
+                pass
+
+            else:
+                player.get_damage(to_attack.strenght, to_attack.crit_chance)
 
     for i, enemy in enumerate(enemies):
         if not enemy.is_alive:
